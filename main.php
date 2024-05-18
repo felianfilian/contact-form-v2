de52151fa35da6e0cf7e9746656a32e918acbe98
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-18 05:33:02 +0200

    send mail logic

--- main.php
+++ main.php
@@ -2,4 +2,14 @@
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $message = $_POST['message'];
+
+$mailHeader = "From:".$name."<".$mail.">\r\n";
+
+$recipient = "frodion@outlook.de";
+
+mail($recipient, "Request", $message, $mailHeader)
+or die("Error!!!");
+
+echo "mail send!";
+
 ?>

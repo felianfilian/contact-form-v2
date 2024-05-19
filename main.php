90ff600c89827c22321b9151d30e8258a9b06e48
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-19 23:39:01 +0200

    thank page update

--- main.php
+++ main.php
@@ -10,6 +10,27 @@ $recipient = "frodion@outlook.de";
 mail($recipient, "Request", $message, $mailHeader)
 or die("Error!!!");
 
-echo "mail send!";
+echo `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Thank you</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <h1>Thank You</h1>
+      <br /><br />
+      <p>Thank you for your message</p>
+      <p>we will contact you as soon as possible</p>
+      <br /><br />
+      <a href="index.html" class="btn-01">back to HOME</a>
+    </div>
+  </body>
+</html>
+
+`;
 
 ?>
